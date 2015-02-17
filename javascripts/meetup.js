961a659aa012a1044280b2aa9ef1eeed36eb3e2f
Andrew Borstein <info@brownborgstudios.com>
2015-02-17 08:44:17 -0500

    Next Meeting - No meeting fix
    When there is NO next meeting on Meetup at all, the content is
    appropriately changed to reflect that (instead of blank or error).

--- javascripts/meetup.js
+++ javascripts/meetup.js
@@ -13,67 +13,108 @@ $.ajax({
 })
     .done(function( xhr, textStatus, response, data, responseJSON ) {
       
-      // First Event in Array from JSON Response
-      var nextEvent = xhr.results[0];
+      // If No Upcoming Event is Posted on Meetup...
+      if (xhr.results[0] == undefined) { 
 
-      // Event URL
-      var eventURL = nextEvent.event_url;
-      document.getElementById("meetupEventURL").href = eventURL;
-      
-      // RSVP Head Count
-      var headCount = nextEvent.yes_rsvp_count;
-      document.getElementById("meetupHeadCount").innerHTML = headCount;
+        document.getElementById("meetupDetails").innerHTML = 'TBD (check back soon)'; // Meeting date and place TBD
+        document.getElementById("meetupPeople").style.display = 'none'; // Don't diplay number of RSVP'ers
+        document.getElementById("meetupEventURL").href = 'http://meetup.com/Rhode-Island-Code-for-America-Brigade/'; // Link to main Meetup page
+        document.getElementById("meetupCTA").innerHTML = 'Join Our Meetup'; // Join us!
+      }
 
-      // RSVP Status of Visitor
-      if (nextEvent.self.rsvp == undefined) {
-        document.getElementById("meetupRSVPStatus").innerHTML = '. What about you?';
-        document.getElementById("meetupCTA").innerHTML = 'RSVP on Meetup';
-      } else {
-        var RSVPstatus = nextEvent.self.rsvp.response;
-        if (RSVPstatus == "yes") {
-          var RSVPstatus = "Going";
-          document.getElementById("meetupRSVPStatus").innerHTML = ', including you. Yay!';
-        }
-        else {
-          var RSVPstatus = "Not Going";
-          document.getElementById("meetupRSVPStatus").innerHTML = ', and you\'ll be missed.';
-          document.getElementById("meetupCTA").innerHTML = 'Change RSVP';
-        }
-      }
-      
-      // Event Date     
-      var now = new Date;
-      var todayNumber = now.getDate();
-      var todayMonth = now.getMonth();
-      var todayTime = formatAMPM(now);
-      var date = new Date(nextEvent.time);
-      var dateTime = formatAMPM(date);
-      var dateDay = date.getDay();
-      var dateNumber = date.getDate();
-      var dateMonth = date.getMonth();
-      var dateYear = date.getFullYear();
-      function formatAMPM(date) {
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var ampm = hours >= 12 ? 'pm' : 'am';
-        hours = hours % 12;
-        hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        var strTime = hours + ':' + minutes + ' ' + ampm;
-        return strTime;
-      }
-      var m_names = new Array("January", "February", "March", 
-      "April", "May", "June", "July", "August", "September", 
-      "October", "November", "December");
-      var d_names = new Array("Sunday", "Monday", "Tuesday", 
-      "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
-      if ( (todayNumber == dateNumber) && (todayMonth == dateMonth) ) {
-        var prettyDate = 'Tonight at ' + dateTime;
-      } 
+      // Otherwise...
       else {
-        var prettyDate = d_names[dateDay] + ', ' + m_names[dateMonth] + ' ' + dateNumber +  ', ' + dateYear + ' at ' + dateTime;
+
+        // NEXT EVENT
+        var nextEvent = xhr.results[0]; // First in the array returned from API
+
+        // EVENT URL
+        var eventURL = nextEvent.event_url;
+        document.getElementById("meetupEventURL").href = eventURL; // Replace button link
+        
+        // RSVP HEADCOUNT
+        var headCount = nextEvent.yes_rsvp_count;
+        document.getElementById("meetupHeadCount").innerHTML = headCount; // Replace number of RSVP'ers
+
+        // RSVP STATUS
+
+        // If Visitor has Not Yet RSVP'ed on Meetup...
+        if (nextEvent.self.rsvp == undefined) {
+          
+          document.getElementById("meetupRSVPStatus").innerHTML = '. What about you?'; // Ammend RSVP'ers note.
+          document.getElementById("meetupCTA").innerHTML = 'RSVP on Meetup'; // Replace button link
+        
+        } 
+
+        // Otherwise...
+        else {
+          
+          // RSVP RESPONSE
+          var RSVPstatus = nextEvent.self.rsvp.response;
+
+          // If RSVP = Yes
+          if (RSVPstatus == "yes") {
+
+            var RSVPstatus = "Going";
+            document.getElementById("meetupRSVPStatus").innerHTML = ', including you. Yay!'; // Ammend RSVP'ers note.
+
+          }
+
+          // If RSVP = No
+          else {
+
+            var RSVPstatus = "Not Going";
+            document.getElementById("meetupRSVPStatus").innerHTML = ', and you\'ll be missed.'; // Ammend RSVP'ers note.
+            document.getElementById("meetupCTA").innerHTML = 'Change RSVP'; // Replace button link
+
+          }
+        }
+        
+        // EVENT DATE
+
+        // Get Today's Date     
+        var now = new Date;
+        var todayNumber = now.getDate();
+        var todayMonth = now.getMonth();
+        var todayTime = formatAMPM(now);
+
+        // Get Event's Date  
+        var date = new Date(nextEvent.time);
+        var dateTime = formatAMPM(date);
+        var dateDay = date.getDay();
+        var dateNumber = date.getDate();
+        var dateMonth = date.getMonth();
+        var dateYear = date.getFullYear();
+
+        // Make the Date Pretty
+        function formatAMPM(date) {
+          var hours = date.getHours();
+          var minutes = date.getMinutes();
+          var ampm = hours >= 12 ? 'pm' : 'am';
+          hours = hours % 12;
+          hours = hours ? hours : 12; // the hour '0' should be '12'
+          minutes = minutes < 10 ? '0' + minutes : minutes;
+          var strTime = hours + ':' + minutes + ' ' + ampm;
+          return strTime;
+        }
+        var m_names = new Array("January", "February", "March", 
+        "April", "May", "June", "July", "August", "September", 
+        "October", "November", "December");
+        var d_names = new Array("Sunday", "Monday", "Tuesday", 
+        "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+        
+        // If event is today...
+        if ( (todayNumber == dateNumber) && (todayMonth == dateMonth) ) {        
+          var prettyDate = 'Tonight at ' + dateTime;    
+        } 
+        // Otherwise...
+        else {
+          var prettyDate = d_names[dateDay] + ', ' + m_names[dateMonth] + ' ' + dateNumber +  ', ' + dateYear + ' at ' + dateTime;
+        
+        }
+        
+        document.getElementById("meetupDate").innerHTML = prettyDate; // Replace the Date
       }
-      document.getElementById("meetupDate").innerHTML = prettyDate;
 
     })
     .fail( function(xhr, textStatus, errorThrown) {
